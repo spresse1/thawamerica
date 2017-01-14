6a616259225d0921184a4369f41701fcf74b68e4
spresse1 <steve@pressers.name>
2017-01-14 09:56:15 -0500

    Begin conversion to pelican-managed site.  Move a bunch of stuff around, add config files, etc, etc.

---
+++ pelicanconf.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Steven Presser'
+SITENAME = u'Thaw America'
+SITEURL = 'https://thawamerica.org'
+
+PATH = 'content'
+PAGE_PATHS = ['']
+ARTICLE_PATHS = ['blog']
+STATIC_PATHS = ['css', 'fonts', 'js', 'static']
+
+ARTICLE_URL = 'blog/{date:%Y}/{date:%b}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
+PAGE_URL = '{slug}.html'
+PAGE_SAVE_AS = '{slug}.html'
+CATEGORY_URL = 'blog/category/{slug}.html'
+CATEGORY_SAVE_AS = 'blog/category/{slug}.html'
+TAG_URL = 'blog/tag/{slug}.html'
+TAG_SAVE_AS = 'blog/tag/{slug}.html'
+AUTHOR_URL='blog/author/{slug}.html'
+AUTHOR_SAVE_AS='blog/author/{slug}.html'
+ARCHIVES_SAVE_AS = 'blog/archives.html'
+AUTHORS_SAVE_AS = 'blog/authors.html'
+CATEGORIES_SAVE_AS = 'blog/categories.html'
+TAGS_SAVE_AS = 'blog/tags.html'
+INDEX_SAVE_AS = 'blog/index.html'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
